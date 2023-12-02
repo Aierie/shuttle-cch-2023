51239a98f09a6ea9781ac1dff21372e5b710bc97
aierie <hockeu.w@gmail.com>
2023-12-02 12:16:16 +0800

    added d1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use tide::{Response, StatusCode};
+use std::{error::Error, ops::BitXor};
+
+use tide::{Request, Response, StatusCode};
 
 #[shuttle_runtime::main]
 async fn tide() -> shuttle_tide::ShuttleTide<()> {
@@ -8,6 +10,25 @@ async fn tide() -> shuttle_tide::ShuttleTide<()> {
     app.at("/").get(|_| async { Ok("Hello, world!") });
     app.at("/-1/error")
         .get(|_| async { Ok(Response::new(StatusCode::InternalServerError)) });
+    app.at("/1/*path").get(|req: Request<()>| async move {
+        let calculated = req
+            .param("path")?
+            .trim_end_matches('/')
+            .split('/')
+            .map(|s| s.parse::<i32>())
+            .reduce(|acc, e| {
+                acc.and_then(|acc_value| e.and_then(|e_value| Ok(acc_value.bitxor(e_value))))
+            })
+            .ok_or(tide::Error::from_str(
+                StatusCode::BadRequest,
+                "No params supplied",
+            ))??
+            .pow(3);
+
+        let mut response = Response::new(StatusCode::Ok);
+        response.set_body(calculated.to_string());
+        Ok(response)
+    });
 
     Ok(app.into())
 }
